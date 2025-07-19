453d2b4d5b5b3004e552325d7c2aa4f85e1165ea
Junel <junelxdmemes123@gmail.com>
2025-07-19 05:41:39 -0500

    un commit para tener todo en orden antes de empezar el trabajo
    para el trabajo

--- Assets/Scripts/HolaMundo.cs
+++ Assets/Scripts/HolaMundo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,14 @@ using UnityEngine;
 public class HolaMundo : MonoBehaviour
 {
 
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
  
         print("algo paso");
-        Debug.Log("Algo paso");
+       
         Debug.LogWarning("Algo salio medianamente mal");
         Debug.LogError("Algo salio muy mal");
     }
@@ -18,7 +21,35 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log(" Hola desde Update ");
+
 
     }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning(" Hola desde FixedUpdate a 50 frames");
+
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log(" Hola desde LateUpdate ");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning(" El Objeto ha sido Habilitado ");
+
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning(" El Objeto ha sido Inhabilitado");
+
+    }
+
 }
